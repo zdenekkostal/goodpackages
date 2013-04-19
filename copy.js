b523d79db31c421bf67f6a45454033d4da0d1f90
Zdenek Kostal <zdenek.kostal@gooddata.com>
2013-04-19 17:09:47 +0200

    jquery replaced for strftime in coffee files

--- copy.js
+++ copy.js
@@ -1,15 +1,18 @@
 var fs = require('fs');
 
+var deps = [];
 var path = './app/packages';
 var files = fs.readdirSync(path + '/coffee');
 
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 100; i++) {
 
     var newPath = path + '/' + 'package' + i + '/';
     fs.mkdirSync(newPath);
 
     var smallPackage = 'package' + i;
     var bigPackage = 'Package' + i;
+
+    deps.push('var ' + bigPackage + ' = ' + 'require("packages/' + smallPackage + '/' + smallPackage + '");');
 
     files.forEach(function(file) {
         var fileStr = fs.readFileSync(path + '/coffee/' + file, 'utf8');
@@ -21,5 +24,9 @@ for (var i = 0; i < 10; i++) {
 
         fs.writeFileSync(newPath + '/' + newFile, fileStr);
     });
+
 }
 
+deps.push('$("body").append("<hr><p>PAGE LOADED!!!</p>");')
+
+fs.writeFileSync('app/pages/index.js', deps.join('\n'))
